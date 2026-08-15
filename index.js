03ec24a37c5adbbc149eb4e2d1e657fa78eb6cba
daahirabshir0-spec <daahirabshir0@gmail.com>
2026-08-15 12:56:10 +0300

    Set Content-Type header for HTML response

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
     res.send(`<!DOCTYPE html>
 <html lang="so">
 <head>
